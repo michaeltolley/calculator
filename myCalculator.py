0c307ef734d798e3c52d974f4c106bbf957de3b7
Michael Tolley <michael@tolleyfam.com>
2020-12-31 11:12:21 -0800

    adding calculator working

--- myCalculator.py
+++ myCalculator.py
@@ -1,1 +1,8 @@
-print("this is my calculator.")
+print("this is my adding calculator.")
+
+firstNumber = int(input("Enter the first number here:  "))
+secondNumber = int(input("Enter the second number here:  "))
+
+sum = firstNumber + secondNumber
+
+print("this is your answer  " + str(sum) )
