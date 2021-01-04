da7700288e8b36769bca4c6720563bb8c8793677
Michael Tolley <michael@tolleyfam.com>
2021-01-04 15:33:51 -0800

    added more operations

--- myCalculator.py
+++ myCalculator.py
@@ -1,8 +1,24 @@
-print("this is my adding calculator.")
+print("this is my calculator.")
+operation = input("chooes an arithmetic operation (add, subtract, multiply, divide): ")
+
 
 firstNumber = int(input("Enter the first number here:  "))
 secondNumber = int(input("Enter the second number here:  "))
 
-sum = firstNumber + secondNumber
+if operation == "add":
+    sum = firstNumber + secondNumber
+    answer = sum
 
-print("this is your answer  " + str(sum) )
+if operation == "difference":
+    difference =  firstNumber - secondNumber
+    answer = difference
+
+if operation == "multiply":
+    product = firstNumber * secondNumber
+    answer = product
+
+if operation == "divide":
+    quotient = firstNumber / secondNumber
+    answer = quotient
+
+print("this is your answer  " + str(answer) )
